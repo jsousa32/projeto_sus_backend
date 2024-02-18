feef9fcbbe11349b677d3a45985cc7818a410367
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-02-18 18:11:18 -0300

    [FIX]: modificação no retorno do jwt

---
+++ src/main/java/api/sus/model/record/AccessToken.java
@@ -0,0 +1,12 @@
+package api.sus.model.record;
+
+/**
+ * The Record AccessToken
+ *
+ * @author João Lucas Silva de Sousa
+ * @sincer 14/02/2024
+ */
+public record AccessToken(
+        String accessToken
+) {
+}
--- src/main/java/api/sus/service/JwtService.java
+++ src/main/java/api/sus/service/JwtService.java
@@ -1,17 +1,20 @@
 package api.sus.service;
 
-import api.sus.exception.GlobalException;
-import api.sus.model.dto.PrincipalDetails;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import api.sus.exception.GlobalException;
+import api.sus.model.dto.PrincipalDetails;
+import api.sus.model.record.AccessToken;
 
 /**
  * The Class JwtService
@@ -25,10 +28,13 @@ public class JwtService {
     private final static String SECRET = "TOKEN";
     private final static String ISSUER = "SUS-Api";
 
-    public String generateToken(PrincipalDetails principalDetails) {
+    public AccessToken generateToken(PrincipalDetails principalDetails) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(SECRET);
-            return JWT.create().withIssuer(ISSUER).withSubject(principalDetails.getEmail()).withExpiresAt(genExpirationDate()).sign(algorithm);
+
+            String token = JWT.create().withIssuer(ISSUER).withSubject(principalDetails.getEmail()).withExpiresAt(genExpirationDate()).sign(algorithm);
+
+            return new AccessToken(token);
 
         } catch (JWTCreationException exception) {
             throw new GlobalException("Erro ao criar o token JWT", HttpStatus.BAD_REQUEST);
