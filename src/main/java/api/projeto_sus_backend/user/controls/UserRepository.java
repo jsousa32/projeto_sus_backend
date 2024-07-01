e82bd53a74a899c9d07c20ca94cef732c9319044
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-01 17:41:20 -0300

    [FEAT]: criação de um repositório para a entidade UserSchema

---
+++ src/main/java/api/projeto_sus_backend/user/controls/UserRepository.java
@@ -0,0 +1,26 @@
+package api.projeto_sus_backend.user.controls;
+
+import api.projeto_sus_backend.user.entities.UserSchema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * The Interface UserRepository
+ *
+ * @author João Lucas Silva de Sousa
+ * @sincer 01/07/2024
+ */
+
+@Transactional
+@Repository
+public interface UserRepository<T extends UserSchema> extends JpaRepository<T, UUID> {
+
+    Optional<T> findByEmail(@Param("email") String email);
+
+    Optional<T> findByDocument(@Param("document") String document);
+}
