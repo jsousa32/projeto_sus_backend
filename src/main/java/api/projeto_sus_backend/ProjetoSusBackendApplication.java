97dadcadcdf515321dd3b86566c02874005bf696
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-01 13:44:41 -0300

    first commit

---
+++ src/main/java/api/projeto_sus_backend/ProjetoSusBackendApplication.java
@@ -0,0 +1,13 @@
+package api.projeto_sus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/projeto_sus_backend/ProjetoSusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package api.projeto_sus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
