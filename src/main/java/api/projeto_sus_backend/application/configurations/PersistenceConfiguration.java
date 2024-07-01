7e2a1081a6f4fd0bcb5363b30ff65a6675ea1a4a
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-01 19:51:17 -0300

    [CONFIG]: adição da configuração da persistência ao projeto

---
+++ src/main/java/api/projeto_sus_backend/application/configurations/PersistenceConfiguration.java
@@ -0,0 +1,15 @@
+package api.projeto_sus_backend.application.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * The Class PersistenceConfiguration
+ *
+ * @author João Lucas Silva de Sousa
+ * @sincer 01/07/2024
+ */
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfiguration {
+}
