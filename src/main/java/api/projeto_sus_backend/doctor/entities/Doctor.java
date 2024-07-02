53547f534da9da33e1dc8f310a2dbd3aa1104910
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-02 14:02:43 -0300

    [FEAT]: criação da entidade e do dto de médicos

---
+++ src/main/java/api/projeto_sus_backend/doctor/entities/Doctor.java
@@ -0,0 +1,99 @@
+package api.projeto_sus_backend.doctor.entities;
+
+import api.projeto_sus_backend.user.entities.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * The Class Doctor
+ *
+ * @author João Lucas Silva de Sousa
+ * @sincer 02/07/2024
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Doctor extends User {
+
+    @NotBlank(message = "Número do CRM obrigatório")
+    @Pattern(regexp = "\\d{6}$", message = "Número do CRM inválido")
+    private String crm;
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    /**
+     * The Builder of Doctor
+     */
+    public static class Builder {
+
+        private Doctor doctor;
+
+        public Builder builder() {
+            this.doctor = new Doctor();
+            return this;
+        }
+
+        public Builder setId(UUID id) {
+            this.doctor.setId(id);
+            return this;
+        }
+
+        public Builder setCrm(String crm) {
+            this.doctor.setCrm(crm);
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.doctor.setFirstName(firstName);
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.doctor.setLastName(lastName);
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.doctor.setEmail(email);
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.doctor.setPassword(password);
+            return this;
+        }
+
+        public Builder setTelephone(String telephone) {
+            this.doctor.setTelephone(telephone);
+            return this;
+        }
+
+        public Builder setDocument(String document) {
+            this.doctor.setDocument(document);
+            return this;
+        }
+
+        public Builder setDisabledAt(LocalDateTime disabledAt) {
+            this.doctor.setDisabledAt(disabledAt);
+            return this;
+        }
+
+        public Builder setDisabled(boolean disabled) {
+            this.doctor.setDisabled(disabled);
+            return this;
+        }
+
+        public Doctor build() {
+            return this.doctor;
+        }
+    }
+}
---
+++ src/main/java/api/projeto_sus_backend/doctor/entities/DoctorSchema.java
@@ -0,0 +1,100 @@
+package api.projeto_sus_backend.doctor.entities;
+
+import api.projeto_sus_backend.pacient.entities.PacientSchema;
+import api.projeto_sus_backend.user.entities.UserSchema;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * The Class DoctorSchema
+ *
+ * @author João Lucas Silva de Sousa
+ * @sincer 02/07/2024
+ */
+@Entity
+@Table(name = "tb_doctors", indexes = {
+        @Index(columnList = "crm")
+})
+@DiscriminatorValue(UserSchema.DESCIMINATOR_DOCTOR)
+public class DoctorSchema extends UserSchema {
+
+    @Column(length = 6, unique = true, nullable = false)
+    private String crm;
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    /**
+     * The Builder of DoctorSchema
+     */
+    public static class Builder {
+
+        private DoctorSchema doctorSchema;
+
+        public Builder builder() {
+            this.doctorSchema = new DoctorSchema();
+            return this;
+        }
+
+        public Builder setId(UUID id) {
+            this.doctorSchema.setId(id);
+            return this;
+        }
+
+        public Builder setCrm(String crm) {
+            this.doctorSchema.setCrm(crm);
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.doctorSchema.setFirstName(firstName);
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.doctorSchema.setLastName(lastName);
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.doctorSchema.setEmail(email);
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.doctorSchema.setPassword(password);
+            return this;
+        }
+
+        public Builder setTelephone(String telephone) {
+            this.doctorSchema.setTelephone(telephone);
+            return this;
+        }
+
+        public Builder setDocument(String document) {
+            this.doctorSchema.setDocument(document);
+            return this;
+        }
+
+        public Builder setDisabledAt(LocalDateTime disabledAt) {
+            this.doctorSchema.setDisabledAt(disabledAt);
+            return this;
+        }
+
+        public Builder setDisabled(boolean disabled) {
+            this.doctorSchema.setDisabled(disabled);
+            return this;
+        }
+
+        public DoctorSchema build() {
+            return this.doctorSchema;
+        }
+    }
+}
