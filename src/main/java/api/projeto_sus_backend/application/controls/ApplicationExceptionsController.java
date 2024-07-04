4c89824a333fde7e323b71ce3a1f038a56967656
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-04 19:46:11 -0300

    [FEAT]: adição de um novo handler

--- src/main/java/api/projeto_sus_backend/application/controls/ApplicationExceptionsController.java
+++ src/main/java/api/projeto_sus_backend/application/controls/ApplicationExceptionsController.java
@@ -2,6 +2,8 @@ package api.projeto_sus_backend.application.controls;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +25,19 @@ public class ApplicationExceptionsController {
                         new ErrorHandling(LocalDateTime.now(), exception.getStatus().value(), request.getServletPath(), request.getMethod(),
                                 exception.getMessage())
                 );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorHandling> handleValidationErrors(MethodArgumentNotValidException exception, HttpServletRequest request) {
+
+        StringBuilder sb = new StringBuilder();
+
+        exception.getBindingResult().getFieldErrors().stream().map(FieldError::getDefaultMessage).forEach(err -> sb.append(err).append(", "));
+
+        return ResponseEntity.status(exception.getStatusCode())
+                .body(
+                        new ErrorHandling(LocalDateTime.now(), exception.getStatusCode().value(), request.getServletPath(), request.getMethod(),
+                                sb.toString()));
     }
 
     public record ErrorHandling(LocalDateTime timestamps, Integer statusCode, String path, String method, String message) {
