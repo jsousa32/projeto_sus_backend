1f5ab26d9228e8c3bdfcfa241ad77334817f511f
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-09 18:38:46 -0300

    [CONFIG]: criação dos beans de configuração do java mail sender

---
+++ src/main/java/api/projeto_sus_backend/application/configurations/MailConfiguration.java
@@ -0,0 +1,47 @@
+package api.projeto_sus_backend.application.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+/**
+ * The Class MailConfiguration
+ *
+ * @author João Lucas Silva de Sousa
+ * @sincer 09/07/2024
+ */
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("noreplyfakesus@gmail.com");
+        javaMailSender.setPassword("bgdenmdthteuyczt");
+        javaMailSender.setJavaMailProperties(getProperties());
+
+        return javaMailSender;
+    }
+
+    @Bean
+    public ResourceLoader resourceLoader() {
+        return new DefaultResourceLoader();
+    }
+
+    private Properties getProperties() {
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        return properties;
+    }
+}
