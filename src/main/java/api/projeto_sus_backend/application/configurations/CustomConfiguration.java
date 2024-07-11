6f2cfc0fb103bfc5139578aae41b460b98912a45
João Sousa <joao.sousa@globalfinanceiro.com.br>
2024-07-11 16:47:43 -0300

    [FIX]: remoção do bean do object mapper

--- src/main/java/api/projeto_sus_backend/application/configurations/CustomConfiguration.java
+++ src/main/java/api/projeto_sus_backend/application/configurations/CustomConfiguration.java
@@ -1,7 +1,6 @@
 package api.projeto_sus_backend.application.configurations;
 
 import api.projeto_sus_backend.application.entities.ModelCustomConfiguration;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
